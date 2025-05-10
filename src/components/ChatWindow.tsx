c552c7c9190704d04300467929b1cb260a8bcd8a
Igoroots34 <igorgdf34@gmail.com>
2025-05-10 14:33:38 -0300

    Create input

--- src/components/ChatWindow.tsx
+++ src/components/ChatWindow.tsx
@@ -6,13 +6,14 @@ export default function ChatWindow() {
   const { messages, sendMessage } = useChat();
 
   return (
-    <div className="flex flex-col h-screen bg-[#030303] text-[#D9D9D9]">
-      <div className="flex-1 overflow-y-auto p-4 space-y-2">
+    <div className="flex flex-col h-screen justify-center items-center bg-[#030303] text-[#D9D9D9]">
+      <div className="flex overflow-y-auto p-4 space-y-2">
         {messages.map((msg) => (
           <MessageBubble key={msg.id} message={msg} />
         ))}
       </div>
-      <MessageInput onSend={sendMessage} />
+      <div ><MessageInput onSend={sendMessage}  /></div>
+      
     </div>
   );
 }
--- src/components/MessageInput.tsx
+++ src/components/MessageInput.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { ArrowDown, SendHorizontal } from 'lucide-react';
 
 interface Props {
   onSend: (text: string) => void;
@@ -15,19 +16,50 @@ export default function MessageInput({ onSend }: Props) {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="bg-chat flex items-start rounded-4xl p-4 border border-[#686868]">
-      <input
-        className="flex-1 rounded px-3 text-sm"
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-        placeholder="Pergunte alguma coisa"
-      />
-      <button
-        type="submit"
-        className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700"
+    <div className="relative">
+      {/* 🔷 Formas decorativas */}
+      <div className="absolute bottom-1 right-1 w-24 h-24 bg-[#E9D362] rounded-full z-0" />
+
+      <form
+        onSubmit={handleSubmit}
+        className="box relative z-10 flex bg-chat flex-col justify-between rounded-4xl p-4 border border-white/20 bg-black/10 backdrop-blur-3xl shadow-lg"
       >
-        Enviar
-      </button>
-    </form>
+        <textarea
+          className="flex rounded px-3 text-sm text-white bg-transparent outline-none resize-none"
+          value={text}
+          rows={3}
+          onChange={(e) => setText(e.target.value)}
+          placeholder="Pergunte alguma coisa"
+        />
+        <div className="flex justify-between mt-4">
+          <div className="flex gap-3">
+            <button
+              type="submit"
+              className="bg-[#121212] border border-[#686868] px-2 py-2 rounded-4xl hover:bg-[#242424]"
+            >
+              <ArrowDown className="text-[#d9d9d9]" />
+            </button>
+            <button
+              type="submit"
+              className="flex gap-2 bg-[#121212] border border-[#686868] text-[#d9d9d9] px-6 py-2 rounded-4xl hover:bg-[#242424]"
+            >
+              Dashboard
+            </button>
+            <button
+              type="submit"
+              className="flex gap-2 text-[#d9d9d9] border border-[#686868] bg-[#121212] px-6 py-2 rounded-4xl hover:bg-[#242424]"
+            >
+              Relatórios
+            </button>
+          </div>
+          <button
+            type="submit"
+            className="bg-[#E9D362] border border-[#686868] px-2 py-2 rounded-full hover:bg-[#e0bb2f]"
+          >
+            <SendHorizontal className="text-[#030303]" />
+          </button>
+        </div>
+      </form>
+    </div>
   );
 }
